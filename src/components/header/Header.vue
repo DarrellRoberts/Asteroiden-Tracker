2ef493e05ceb7b5c47dfd2d9d3def430dd2f780b
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-11-01 20:35:55 +0100

    0005:header-skeleton wie buttons hinzugefügt

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -6,7 +6,19 @@ defineProps({
 </script>
 
 <template>
-  <h1>Asteroiden Tracker</h1>
+  <div class="header-container">
+    <h1>Asteroiden Tracker</h1>
+    <div class="button-container">
+      <button>Randomise</button>
+      <button>Filter</button>
+    </div>
+    <div class="navigation-container">
+      <span>Previous</span>
+      <span>Next</span>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../../assets/stylesheets/components/headercontainer";
+</style>
